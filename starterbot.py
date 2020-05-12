ab57f679baa6fe9160da0ece5fda604b38f0df81
saleh <saleh@alghusson.com>
2020-05-12 03:51:32 -0400

    tests

--- starterbot.py
+++ starterbot.py
@@ -43,6 +43,7 @@ def handle_command(command, channel):
     """
         Executes bot command if the command is known
     """
+    print('here')
     # Default response is help text for the user
     response = "Not sure what you mean. Try *{}*.".format(EXAMPLE_COMMAND)
 
